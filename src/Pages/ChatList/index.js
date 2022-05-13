befc5d587717a86fc87b7639a395fec46dafc06d
mohammad maleki <1mohamad.maleki@gmail.com>
2022-05-14 02:43:22 +0430

    add chat list

---
+++ src/Pages/ChatList/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ChatListItem from '../ChatListItem'
+
+const ChatList = () => {
+  return (
+    <div className="bg-zinc-800 w-5/12">
+      <ChatListItem />  
+      <ChatListItem />  
+      <ChatListItem />  
+    </div>
+  )
+}
+
+export default ChatList
