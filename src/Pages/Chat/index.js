51e9e092aff6f3158829bf874c3079407552869f
mohammad maleki <1mohamad.maleki@gmail.com>
2022-05-14 02:44:37 +0430

    add chat side bar

--- src/Pages/Chat/index.js
+++ src/Pages/Chat/index.js
@@ -1,1 +1,24 @@
-export {default} from './Chat'
+import React from 'react'
+import Message from './../../Components/Messages'
+import OppositeUser from './../../Components/OppositeUser'
+import SendMessage from './../../Components/SendMessage'
+import ChatList from '../ChatList'
+import ChatSidebar from '../ChatSidebar'
+
+const Chat = () => {
+    return (
+        
+        <div class="flex">
+                <ChatSidebar />
+                <ChatList />
+            <div class="flex flex-col justify-between w-7/12 min-w-xl h-screen mx-auto w bg-chat_body">
+                <OppositeUser />
+                <Message/>
+                <SendMessage/>
+            </div>
+        </div>
+        
+    )
+}
+
+export default Chat
---
+++ src/Pages/ChatSidebar/index.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react'
+import img from "../../avatar.png"
+const ChatMenu = () => {
+  
+    const [showSidebar, setShowSidebar] = useState(false);
+
+    return (
+      <div className="bg-zinc-900 h-full w-[7%]">
+        <div className={`h-full ${showSidebar ? 'fixed w-full bg-[rgba(0,0,0,.7)] duration-300 opacity-100' : 'opacity-0'}`}  onClick={() => setShowSidebar(false)}></div>
+      <div className="flex flex-col items-center justify-center min-h-screen py-2">
+          <svg
+            onClick={() => setShowSidebar(true)}
+            className="fixed  z-30 flex items-center cursor-pointer left-6 top-4"
+            fill="#2563EB"
+            viewBox="0 0 100 80"
+            width="35"
+            height="35"
+          >
+            <rect width="100" height="10"></rect>
+            <rect y="30" width="100" height="10"></rect>
+            <rect y="60" width="100" height="10"></rect>
+          </svg>
+        
+  
+        <div
+          className={`top-0 left-0 w-[20vw] bg-slate-800 pt-6 text-white fixed h-full z-40 ease-in-out duration-300 ${
+            showSidebar ? "translate-x-0" : "-translate-x-full"
+          }`}
+        >
+          <div className="flex flex-col pb-5 pl-5 border-b-2 border-slate-900">
+            <img src={img} alt="avatar" className="w-1/3" />
+              <h1 className="py-1 text-lg">mohammad</h1>
+              <p className="text-blue-300 text-sm">09333333333</p>
+          </div>
+
+          <ul className="flex flex-col pt-5">
+            <li className="flex space-x-2 px-5 py-3.5 hover:bg-slate-700 ease-in-out duration-200 cursor-pointer">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                  <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
+              </svg>
+              <p>تنظیمات</p>
+            </li>
+            <li className="flex space-x-2 px-5 py-3.5 hover:bg-slate-700 ease-in-out duration-200 cursor-pointer">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                  <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
+              </svg>
+              <p>تنظیمات</p>
+            </li>
+            <li className="flex space-x-2 px-5 py-3.5 hover:bg-slate-700 ease-in-out duration-200 cursor-pointer">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                  <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
+              </svg>
+              <p>تنظیمات</p>
+            </li>
+            <li className="flex space-x-2 px-5 py-3.5 hover:bg-slate-700 ease-in-out duration-200 cursor-pointer">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                  <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
+              </svg>
+              <p>تنظیمات</p>
+            </li>
+          </ul>
+        </div>
+        </div>
+        </div>
+    );
+  };
+  
+export default ChatMenu
