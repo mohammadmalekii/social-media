d03e5416ad3c6757d7efba52c06e10eb47c192ef
mohammad maleki <1mohamad.maleki@gmail.com>
2022-06-13 12:47:16 +0430

    route login and home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import './App.css'
-import Home from './Pages/Home'
+import "./App.css";
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <>
-      <Home />
-    </>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
